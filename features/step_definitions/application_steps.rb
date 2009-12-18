ec4f50b08c31fdb5283d4355284cbf09f814515d
Mr. Matt <matt@smalls.(none)>
2009-12-18 16:36:09 -0700

    cucumbers

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -1,1 +1,12 @@
-Given
+Given /^I am a signed in (.*) user named (.*)$/ do |role, login|
+	is_admin = ( role.include?( "admin" ) ? true : false )
+  user = Factory( :user, :login => login, :is_admin => is_admin, :password => "testpass", :password_confirmation => "testpass" )
+
+	post "/login", :login => login, :password => "testpass"
+end
+
+Given /^the following (.+) records:$/ do |factory, table|
+  table.hashes.each do |hash|
+    Factory(factory, hash)
+  end
+end
--- features/step_definitions/invoice_steps.rb
+++ features/step_definitions/invoice_steps.rb
@@ -1,3 +1,3 @@
-Given /^There is a client named (.*)$/ do
-  Factory
+Given /^There is a client named (.*)$/ do |name|
+  Factory( :client, :name => name )
 end
